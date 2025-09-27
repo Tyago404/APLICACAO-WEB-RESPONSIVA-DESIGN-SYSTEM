fa9f5275902cc76e459a69fbca53897e4b183383
Tiago Santos <tiago40427@gmail.com>
2025-09-26 23:52:55 -0300

    Header responsivo

--- layout-responsive/src/components/Header/index.tsx
+++ layout-responsive/src/components/Header/index.tsx
@@ -12,8 +12,8 @@ export function Header() {
       )}
     >
       <div className="flex gap-6 mx-2 md:hidden">
-        <Menu className="hover:scale-105 cursor-pointer" />
-        <Search className="hover:scale-105 cursor-pointer" />
+        <Menu className="hover:scale-105 cursor-pointer" size={25} />
+        <Search className="hover:scale-105 cursor-pointer" size={25} />
       </div>
 
       <div className="flex">
@@ -22,9 +22,9 @@ export function Header() {
       </div>
 
       <div className="flex items-center gap-6">
-        <Input placeholder="Pesquisar" classname="hidden md:block"/>
-        <ShoppingCart className="hover:scale-105 cursor-pointer " />
-        <UserRound className="hover:scale-105 cursor-pointer" />
+        <Input placeholder="Pesquisar" classname="hidden md:block w-45"/>
+        <ShoppingCart className="hover:scale-105 cursor-pointer" size={25} />
+        <UserRound className="hover:scale-105 cursor-pointer" size={25} />
       </div>
     </header>
   );
--- layout-responsive/src/components/Logo/index.tsx
+++ layout-responsive/src/components/Logo/index.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export function Logo() {
   return (
     <>
-      <Image className="md:w-20 md:h-20 m-2" src="/images/RifflyLogo.png" alt="Logo" width={50} height={50} priority/>
+      <Image
+        className="md:w-20 md:h-20 m-2 hover:scale-105 cursor-pointer"
+        src="/images/RifflyLogo.png"
+        alt="Logo"
+        width={50}
+        height={50}
+        priority
+      />
     </>
   );
 }
